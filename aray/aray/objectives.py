d7d7d3fd1a43177da873da6b79c919751297da08
A Ray <a@machinaut.com>
2021-07-09 15:34:41 -0400

    tests for stretch loss

--- aray/aray/objectives.py
+++ aray/aray/objectives.py
@@ -20,15 +20,15 @@ We compute this for edge ``a, b`` with ``stretch = max(abs(d(a', b') / d(a, b) -
 '''
 
 # %% imports
+from typing import List
 import torch
 
+from aray.problem import Edge
+
+
 # %% distance function
-
-
 def dist(a: torch.tensor, b: torch.tensor) -> torch.tensor:
-    '''
-    Squared distance function for points a and b.
-    '''
+    ''' Squared distance function for points a and b. '''
     # assert a and b have same shape, and last dim is 2
     assert a.shape == b.shape, f'a and b have different shapes: {a.shape} and {b.shape}'
     assert a.shape[-1] == 2, f'a and b have last dim != 2: {a.shape}'
@@ -39,4 +39,18 @@ def dist(a: torch.tensor, b: torch.tensor) -> torch.tensor:
 
 
 # %%
-# def loss_stretch(problem, solution):
+def loss_stretch(edges: List[Edge], original: torch.tensor, current: torch.tensor, epsilon: int):
+    ''' Calculate the total stretch loss for a problem '''
+    # assert original and current have 2 dims, same shape, and second dim is 2
+    assert original.shape == current.shape, f'{original.shape} vs {current.shape}'
+    assert original.shape[-1] == 2, f'last dim != 2: {original.shape}'
+    assert len(original.shape) == 2, f'{original.shape}'
+
+    ratio = torch.empty(len(edges), dtype=torch.float)
+    for i, (v1, v2) in enumerate(edges):
+        d_orig = dist(original[v1], original[v2])
+        d_curr = dist(current[v1], current[v2])
+        ratio[i] = d_curr / d_orig
+    
+    stretch = torch.max(torch.abs(ratio - 1) * 1e6 - epsilon, torch.zeros_like(ratio))
+    return torch.sum(stretch)
--- aray/tests/test_objectives.py
+++ aray/tests/test_objectives.py
@@ -4,7 +4,7 @@
 import unittest
 import torch
 
-from aray.objectives import dist
+from aray.objectives import dist, loss_stretch
 
 
 class TestDistance(unittest.TestCase):
@@ -24,6 +24,43 @@ class TestDistance(unittest.TestCase):
         b = torch.Tensor([3, 4])
         self.assertEqual(dist(a, b).tolist(), 25.)
 
+
+class TestStretch(unittest.TestCase):
+    def test_basic_bigger(self):
+        edges = [[0, 1]]
+        original = torch.tensor([[0, 0], [0, 10]], dtype=torch.float)
+        current = torch.tensor([[0, 0], [0, 11]], dtype=torch.float)
+        epsilon = 0
+        loss = loss_stretch(edges, original, current, epsilon)
+        correct = abs((11**2 / 10**2) - 1) * 1e6
+        self.assertAlmostEqual(loss.tolist(), correct, places=1)
+
+    def test_basic_smaller(self):
+        edges = [[0, 1]]
+        original = torch.tensor([[0, 0], [0, 2]], dtype=torch.float)
+        current = torch.tensor([[0, 0], [0, 1]], dtype=torch.float)
+        epsilon = 0
+        loss = loss_stretch(edges, original, current, epsilon)
+        correct = abs((1**2 / 2**2) - 1) * 1e6
+        self.assertAlmostEqual(loss.tolist(), correct, places=1)
+
+    def test_basic_epsilon(self):
+        edges = [[0, 1]]
+        original = torch.tensor([[0, 0], [0, 2]], dtype=torch.float)
+        current = torch.tensor([[0, 0], [0, 1]], dtype=torch.float)
+        epsilon = 500_000
+        loss = loss_stretch(edges, original, current, epsilon)
+        correct = abs((1**2 / 2**2) - 1) * 1e6 - epsilon
+        self.assertAlmostEqual(loss.tolist(), correct, places=1)
+
+    def test_basic_zero(self):
+        edges = [[0, 1]]
+        original = torch.tensor([[0, 0], [0, 2]], dtype=torch.float)
+        current = torch.tensor([[0, 0], [0, 1]], dtype=torch.float)
+        epsilon = 1_000_000
+        loss = loss_stretch(edges, original, current, epsilon)
+        correct = 0.0
+        self.assertAlmostEqual(loss.tolist(), correct, places=1)
 
 
 if __name__ == '__main__':

2d649fba64d3f943673fa75cbc55656427073871
A Ray <a@machinaut.com>
2021-07-09 20:39:40 -0400

    visualizing losses

--- aray/aray/objectives.py
+++ aray/aray/objectives.py
@@ -20,7 +20,8 @@ def dist(a: torch.tensor, b: torch.tensor) -> torch.tensor:
 def near(A: torch.tensor, B: torch.tensor, P: torch.tensor):
     """ Get the distance to the nearest point to P on line segment A-B """
     # https://math.stackexchange.com/a/3128850
-    assert A.shape == B.shape == P.shape == (2,) , f'{A.shape} {B.shape} {P.shape}'
+    assert A.shape == B.shape == P.shape == (2,), \
+        f'{A.shape} {B.shape} {P.shape}'
     v = B - A
     u = A - P
     vu = (v * u).sum()
@@ -76,26 +77,27 @@ def loss_dislikes(hole: torch.tensor, current: torch.tensor, temperature: float)
 
 
 def loss_barrier(edges: List[Edge], hole: torch.tensor, current: torch.tensor) -> torch.tensor:
-    assert hole.shape[-1] == 2, f'{hole.shape}'
-    assert current.shape[-1] == 2, f'{current.shape}'
-    assert len(hole.shape) == 2, f'{hole.shape}'
-    assert len(current.shape) == 2, f'{current.shape}'
-    assert len(edges) > 0, f'{edges}'
+    # assert hole.shape[-1] == 2, f'{hole.shape}'
+    # assert current.shape[-1] == 2, f'{current.shape}'
+    # assert len(hole.shape) == 2, f'{hole.shape}'
+    # assert len(current.shape) == 2, f'{current.shape}'
+    # assert len(edges) > 0, f'{edges}'
 
-    # make a list of hole edges to match
-    hole_edges = list(
-        zip(range(len(hole) - 1), range(1, len(hole)))) + [(len(hole) - 1, 0)]
+    # # make a list of hole edges to match
+    # hole_edges = list(
+    #     zip(range(len(hole) - 1), range(1, len(hole)))) + [(len(hole) - 1, 0)]
 
-    # construct this matrix sparsely, and we'll set to 1 everything beyond our limit = 1.0 unit
-    distances = torch.empty((len(edges), len(current)), dtype=torch.float)
-    for i, (v1, v2) in enumerate(edges):
-        p1 = hole[v1]
-        p2 = hole[v2]
-        for j, p0 in enumerate(current):
-            out = outside(p1, p2, p0)
+    # # construct this matrix sparsely, and we'll set to 1 everything beyond our limit = 1.0 unit
+    # distances = torch.empty((len(edges), len(current)), dtype=torch.float)
+    # for i, (v1, v2) in enumerate(edges):
+    #     p1 = hole[v1]
+    #     p2 = hole[v2]
+    #     for j, p0 in enumerate(current):
+    #         out = outside(p1, p2, p0)
 
-    # clip the distances to be in -1..0
-    distances = torch.clamp(distances, -1.0, 0.0)
+    # # clip the distances to be in -1..0
+    # distances = torch.clamp(distances, -1.0, 0.0)
 
-    # add the barrier loss
-    return torch.sum(torch.log(-distances))
+    # # add the barrier loss
+    # return torch.sum(torch.log(-distances))
+    pass

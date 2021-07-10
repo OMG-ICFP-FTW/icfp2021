5f3e9830003e165295b19f632bb9a39f452ecdd2
A Ray <a@machinaut.com>
2021-07-10 19:39:55 -0400

    boxlets work now

--- aray/scripts/plot_partition.py
+++ aray/scripts/plot_partition.py
@@ -20,9 +20,18 @@ from aray.problem import Problem
 # polygon = [Point(10, 0), Point(0, 10), Point(10, 10), Point(4, 8)]
 number = random.randint(1,79)
 polygon = Problem.get(number).hole
+# polygon = [Point(x=0.9777859405780998, y=7.931598295985899),
+#  Point(x=6.095741485892159, y=7.912919084690875),
+#  Point(x=5.523774597615163, y=4.099890753524409),
+#  Point(x=4.915330592635722, y=7.8768037617591355)]
+# polygon = [Point(x=2.7211796489261006, y=7.606768544504109),
+#  Point(x=8.66436658931826, y=9.238654894097543),
+#  Point(x=3.9793588430828732, y=5.920407912946276),
+#  Point(x=2.7852902459037363, y=1.6504715885934695)]
 
-M = max(p.x for p in polygon)
-N = max(p.y for p in polygon)
+
+M = max(int(max(p.x for p in polygon) + 1), 10)
+N = max(int(max(p.y for p in polygon) + 1), 10)
 
 fig, ax = plt.subplots()
 ax.set_xlim(-1, M)
@@ -34,7 +43,7 @@ ax.grid(which='major', axis='y', linewidth=0.75, color='k', alpha=0.1)
 ax.set_aspect('equal')
 
 cycle = polygon + [polygon[0]]
-ax.plot([c.x for c in cycle], [c.y for c in cycle], 'k:')
+# ax.plot([c.x for c in cycle], [c.y for c in cycle], 'k-')
 
 def plot_quad(quad, ax):
     """ Plot a quad """
@@ -44,7 +53,7 @@ def plot_quad(quad, ax):
     ys = [p.y for p in points]
     ax.fill(xs, ys, alpha=0.5)
     # also plot the edges with points
-    ax.plot(xs, ys, 'ko-', linewidth=0.5)
+    ax.plot(xs, ys, 'k-', linewidth=0.5)
 
 
 quads = partition(polygon)

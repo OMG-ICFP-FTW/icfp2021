581bedd2e63dd81efb66b519cefbf2a7fbf82287
A Ray <a@machinaut.com>
2021-07-10 22:23:21 -0400

    stretch

--- aray/aray/stretch.py
+++ aray/aray/stretch.py
@@ -9,19 +9,55 @@ from .util import ceil, floor
 from .types import Point
 
 # %%
-def stretch(center: Point, outer_radius: float, inner_radius: float) -> List[Point]:
-    """ Get the set of points that are between inner_radius and outer_radius away from center """
-    # Do this the simple way, where we evaluate every single point
-    # and then filter out the ones that are outside the outer circle,
-    # or inside the inner circle
-    xmin = ceil(center.x - outer_radius)
-    xmax = floor(center.x + outer_radius)
-    ymin = ceil(center.y - outer_radius)
-    ymax = floor(center.y + outer_radius)
+
+
+def stretch(start: Point, d_old: int, epsilon: int) -> List[Point]:
+    """ Get the valid set of points for a given start and old squared distance """
+    outer = (1 + epsilon / 1e6) * (d_old ** 0.5)
+    inner = (1 - epsilon / 1e6) * (d_old ** 0.5)
+    # print('calculated outer', outer, 'inner', inner)
+    x_outer_min = ceil(start.x - outer)
+    x_outer_max = floor(start.x + outer)
+    x_inner_min = floor(start.x - inner)
+    x_inner_max = ceil(start.x + inner)
+    # print('xs', x_outer_min, x_inner_min, x_inner_max, x_outer_max)
     points = []
-    for x in range(xmin, xmax + 1):
-        for y in range(ymin, ymax + 1):
-            r = (x - center.x)**2 + (y - center.y)**2
-            if r <= outer_radius**2 and r >= inner_radius**2:
+    for x in range(x_outer_min, x_outer_max + 1):
+        delta_x = abs(x - start.x)
+        # print('x', x, 'delta x', delta_x)
+        if x <= x_inner_min or x >= x_inner_max:
+            assert delta_x > inner
+            # calculate the y values on outer circle for this x
+            delta_y = (outer ** 2 - delta_x ** 2) ** 0.5
+            y_outer_max = floor(start.y + delta_y)
+            y_outer_min = ceil(start.y - delta_y)
+            # print('delta y', delta_y)
+            # print('y max', start.y + delta_y)
+            # print('y min', start.y - delta_y)
+            # print('y_outer_max', y_outer_max)
+            # print('y_outer_min', y_outer_min)
+            for y in range(y_outer_min, y_outer_max + 1):
+                points.append(Point(x, y))
+        else:
+            assert delta_x <= inner, f'{delta_x} {inner}'
+            # only calculate the y values between outer and inner circles
+            delta_y_outer = abs(outer ** 2 - delta_x ** 2) ** 0.5
+            delta_y_inner = abs(inner ** 2 - delta_x ** 2) ** 0.5
+            # print('delta y outer', delta_y_outer)
+            # print('delta y inner', delta_y_inner)
+            y_outer_max = floor(start.y + delta_y_outer)
+            y_outer_min = ceil(start.y - delta_y_outer)
+            y_inner_max = ceil(start.y + delta_y_inner)
+            y_inner_min = floor(start.y - delta_y_inner)
+            # print('y max outer', start.y + delta_y_outer)
+            # print('y min outer', start.y - delta_y_outer)
+            # print('y max inner', start.y + delta_y_inner)
+            # print('y min inner', start.y - delta_y_inner)
+            # print('ys', y_outer_min, y_inner_min, y_inner_max, y_outer_max)
+            # assert y_outer_max >= y_inner_max, f'{start} {d_old} {epsilon}'
+            # assert y_outer_min <= y_inner_min, f'{start} {d_old} {epsilon}'
+            for y in range(y_outer_min, y_inner_min + 1):
+                points.append(Point(x, y))
+            for y in range(y_inner_max, y_outer_max + 1):
                 points.append(Point(x, y))
     return points
--- aray/scripts/plot_stretch.py
+++ aray/scripts/plot_stretch.py
@@ -4,78 +4,77 @@
 from math import sin, cos
 from typing import List
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from aray.util import ceil, floor
 from aray.types import Point
 from aray.stretch import stretch
 
 
-# %%
 import random
-center = Point(random.uniform(3, 7), random.uniform(3, 7))
-inner, outer = sorted((random.uniform(1, 6), random.uniform(1, 10)))
-assert inner <= outer
 
-import numpy as np
-import matplotlib.pyplot as plt
+for fname in ['uniform', 'randint']:
+    fn = getattr(random, fname)
+    fig, axes = plt.subplots(2, 2, figsize=(8, 8))
+    for ax in axes.flat:
+        center = Point(fn(3, 7), fn(3, 7))
+        # print('center', center)
+        r_old = fn(2, 4)
+        frac = random.uniform(0.2, 0.3)
+        inner = r_old * (1 - frac)
+        outer = r_old * (1 + frac)
+        d_old = r_old ** 2
+        epsilon = int(frac * 1e6)
+        assert inner <= outer
+        # print('r', r_old, 'inner', inner, 'outer', outer)
 
-xs = np.linspace(0, 10, 10000)
-outer_upper_ys = np.sqrt(outer**2 - (xs - center.x)**2) + center.y
-outer_lower_ys = -np.sqrt(outer**2 - (xs - center.x)**2) + center.y
-inner_upper_ys = np.sqrt(inner**2 - (xs - center.x)**2) + center.y
-inner_lower_ys = -np.sqrt(inner**2 - (xs - center.x)**2) + center.y
-
-# plot the circles and the center
-fig, ax = plt.subplots()
-
-ax.set_xlim(-1, 11)
-ax.set_ylim(-1, 11)
-ax.set_xticks(range(11))
-ax.set_yticks(range(11))
-ax.grid(which='major', axis='x', linewidth=0.75, color='k', alpha=0.1)
-ax.grid(which='major', axis='y', linewidth=0.75, color='k', alpha=0.1)
-ax.set_aspect('equal')
-
-ax.plot(xs, outer_upper_ys, 'b-')
-ax.plot(xs, outer_lower_ys, 'b-')
-ax.plot(xs, inner_upper_ys, 'r-')
-ax.plot(xs, inner_lower_ys, 'r-')
-ax.plot([center.x], [center.y], 'gx')
-
-points = stretch(center, outer, inner)
-ax.scatter([p.x for p in points], [p.y for p in points])
+        xs = np.linspace(0, 10, 10000)
+        outer_upper_ys = np.sqrt(outer**2 - (xs - center.x)**2) + center.y
+        outer_lower_ys = -np.sqrt(outer**2 - (xs - center.x)**2) + center.y
+        inner_upper_ys = np.sqrt(inner**2 - (xs - center.x)**2) + center.y
+        inner_lower_ys = -np.sqrt(inner**2 - (xs - center.x)**2) + center.y
 
 
-# %%
-import random
-center = Point(random.randint(3, 7), random.randint(3, 7))
-inner, outer = sorted((random.randint(1, 6), random.randint(1, 10)))
-assert inner <= outer
+        ax.set_xlim(-1, 11)
+        ax.set_ylim(-1, 11)
+        ax.set_xticks(range(11))
+        ax.set_yticks(range(11))
+        ax.grid(which='major', axis='x', linewidth=0.75, color='k', alpha=0.1)
+        ax.grid(which='major', axis='y', linewidth=0.75, color='k', alpha=0.1)
+        ax.set_aspect('equal')
 
-import numpy as np
-import matplotlib.pyplot as plt
+        ax.plot(xs, outer_upper_ys, 'b-')
+        ax.plot(xs, outer_lower_ys, 'b-')
+        ax.plot(xs, inner_upper_ys, 'r-')
+        ax.plot(xs, inner_lower_ys, 'r-')
+        ax.plot([center.x], [center.y], 'gx')
 
-xs = np.linspace(0, 10, 10000)
-outer_upper_ys = np.sqrt(outer**2 - (xs - center.x)**2) + center.y
-outer_lower_ys = -np.sqrt(outer**2 - (xs - center.x)**2) + center.y
-inner_upper_ys = np.sqrt(inner**2 - (xs - center.x)**2) + center.y
-inner_lower_ys = -np.sqrt(inner**2 - (xs - center.x)**2) + center.y
+        points = stretch(center, d_old, epsilon)
+        # print(points)
+        ax.scatter([p.x for p in points], [p.y for p in points])
 
-# plot the circles and the center
-fig, ax = plt.subplots()
 
-ax.set_xlim(-1, 11)
-ax.set_ylim(-1, 11)
-ax.set_xticks(range(11))
-ax.set_yticks(range(11))
-ax.grid(which='major', axis='x', linewidth=0.75, color='k', alpha=0.1)
-ax.grid(which='major', axis='y', linewidth=0.75, color='k', alpha=0.1)
-ax.set_aspect('equal')
+        # TODO: use this for checking
+        def stretch_check(center: Point, outer_radius: float, inner_radius: float) -> List[Point]:
+            """ Get the set of points that are between inner_radius and outer_radius away from center """
+            # Do this the simple way, where we evaluate every single point
+            # and then filter out the ones that are outside the outer circle,
+            # or inside the inner circle
+            xmin = ceil(center.x - outer_radius)
+            xmax = floor(center.x + outer_radius)
+            ymin = ceil(center.y - outer_radius)
+            ymax = floor(center.y + outer_radius)
+            points = []
+            for x in range(xmin, xmax + 1):
+                for y in range(ymin, ymax + 1):
+                    r = (x - center.x)**2 + (y - center.y)**2
+                    if r <= outer_radius**2 and r >= inner_radius**2:
+                        points.append(Point(x, y))
+            return points
 
-ax.plot(xs, outer_upper_ys, 'b-')
-ax.plot(xs, outer_lower_ys, 'b-')
-ax.plot(xs, inner_upper_ys, 'r-')
-ax.plot(xs, inner_lower_ys, 'r-')
-ax.plot([center.x], [center.y], 'gx')
+        check_points = stretch_check(center, outer, inner)
 
-points = stretch(center, outer, inner)
-ax.scatter([p.x for p in points], [p.y for p in points])
+        # check that points are the same
+        assert len(points) == len(check_points)
+        assert set(points) == set(check_points), f'{set(points) - set(check_points)}'

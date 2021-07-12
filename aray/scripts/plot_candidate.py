dd13954c644c655d16cf2d515985eaefc80a8bcd
A Ray <a@machinaut.com>
2021-07-11 22:08:25 -0400

    bad candidates

--- aray/scripts/plot_candidate.py
+++ aray/scripts/plot_candidate.py
@@ -14,3 +14,18 @@ with open(fname, 'r') as f:
     pose = Pose.from_json(json.load(f))
 print(pose)
 print(problem)
+
+# %%
+fig, ax = plt.subplots()
+
+# flip y axis
+ax.invert_yaxis()
+
+for a, b in problem.edges:
+    u, v = pose.vertices[a], pose.vertices[b]
+    ax.plot([u.x, v.x], [u.y, v.y], 'b-')
+
+cycle = problem.hole + [problem.hole[0]]
+for i in range(len(cycle) - 1):
+    u, v = cycle[i], cycle[i + 1]
+    ax.plot([u.x, v.x], [u.y, v.y], 'r-')
